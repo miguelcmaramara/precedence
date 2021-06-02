40c05fd44c0f7a259eb935e56ce3b876a22866e9
miguelcmaramara <miguelcmaramara@gmail.com>
2021-06-01 22:53:08 -0400

    changed requests to GET HTML directly rather than JSON files

--- static/js/requests.js
+++ static/js/requests.js
@@ -2,18 +2,40 @@ console.log("Hello World");
 
 var get_URL="http://127.0.0.1:5000" // Unused just in case
 
-/**
- * Function that gets all tasks, visible or not
- */
+/* 
+//Depreciated function to get tasks as json rather than html. Kept as reference
 function getTasks(){
-    let xhr = new XMLHttpRequest
-    xhr.open('GET', "/request/all_tasks", true)
-    xhr.onload = function()
-        {
+    let xhr = new XMLHttpRequest;
+    xhr.open('GET', "/request/all_tasks", true);
+    xhr.onload = function() {
             if (this.status ===200){
-                console.log(JSON.parse(this.responseText))
+                console.log(JSON.parse(this.responseText));
+                return JSON.parse(this.responseText);
             }
         }
 
-    xhr.send()
+    xhr.send();
 }
+// */
+
+function getTasks(){
+    let xhr = new XMLHttpRequest;
+    xhr.open('GET', "/request/all_tasks", true);
+    xhr.onload = function() {
+            if (this.status ===200){
+                // remove child nodes
+                // insert new nodes
+                console.log(this);
+                console.log(this.responseText);
+                var taskList = document.getElementById("task-container");
+                taskList.innerHTML = this.responseText;
+                // taskList.appendChild(this.responseText);
+
+
+            }
+        }
+
+    xhr.send();
+}
+
+getTasks();
